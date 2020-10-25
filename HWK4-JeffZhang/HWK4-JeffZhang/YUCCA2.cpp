91ebcbb1aa58d525d4004a1304212390f4e0a70a
Jeff Zhang <jz1766american.edu>
2020-10-25 18:54:01 -0400

    Updated YUCCA.cpp

--- HWK4-JeffZhang/HWK4-JeffZhang/YUCCA2.cpp
+++ HWK4-JeffZhang/HWK4-JeffZhang/YUCCA2.cpp
@@ -1,13 +1,11 @@
-//#include <stdio.h>
-//#include <stdbool.h>
-//#include<stdlib.h>
 #include<iostream>
 #define NODE 9
-int graph[NODE][NODE] = { {0, 1, 1, 0, 1},
-{1, 0, 1, 0, 0},
-{0, 1, 0, 0, 1},
-{1, 1, 1, 0, 1},
-{0, 1, 1, 0, 0} };
+
+int graph[NODE][NODE] = { {0, 1, 1, 0},
+						  {1, 0, 1, 1},
+						  {0, 0, 0, 0},
+						  {1, 0, 1, 0} };
+
 void traverse(int u, bool visited[]) {
 	visited[u] = true; //mark v as visited
 	for (int v = 0; v < NODE; v++) {
@@ -17,9 +15,10 @@ void traverse(int u, bool visited[]) {
 		}
 	}
 }
-bool isConnected() {
-	bool* vis = malloc(NODE * sizeof(bool)); //for all vertex u as start point, check whether all nodes are visible or not
-	for (int u; u < NODE; u++) {
+
+bool isYUPPA() {
+	bool* vis = new bool[NODE]; //for all vertex u as start point, check whether all nodes are visible or not
+	for (int u = 0; u < NODE; u++) {
 		for (int i = 0; i < NODE; i++)
 			vis[i] = false;   //initialize as no node is visited
 		traverse(u, vis);
@@ -31,7 +30,7 @@ bool isConnected() {
 	return true;
 }
 int main() {
-	if (isConnected())
+	if (isYUPPA())
 		printf("The Graph is YUCCA.");
 	else
 		printf("The Graph is NOT YUCCA.");
